409905382b0212081e77dbc58336a4ac3a782a2d
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-01-27 22:52:07 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
-local S&G = 11767933308
+local SEG = 11767933308
 local GameID = game.PlaceId
 
-if (GameID == S&G) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Patooh777/WickedHub/Main/Smash%26Grab.lua"))()
+if (GameID == SEG) then
+    loadstring(game:HttpGet("https://github.com/Patooh777/WickedHub/blob/Main/Smash%26Grab.lua?raw=true"))()
 else
     print("invalid Place")
     game.Players.LocalPlayer:Kick("Ur script dont support this shit ass game")
