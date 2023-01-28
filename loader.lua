a2b6243895acc244549086f93894264a1e6849d1
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-01-27 21:54:31 -0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,9 @@
+local S&G = 11767933308
+local GameID = game.PlaceId
+
+if (GameID == S&G) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Patooh777/Swordman-Simulator/SunsetXHub/README.md"))()
+else
+    print("invalid Place")
+    game.Players.LocalPlayer:Kick("Ur script dont support this shit ass game")
+end
