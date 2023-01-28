7d82b5ae4961608eb909121dd388ab3f5b8e6149
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-01-28 16:26:24 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,13 @@
 local SEG = 11767933308
+local CARTYC = 11874473440
 local GameID = game.PlaceId
 
 if GameID == SEG then
     loadstring(game:HttpGet("https://github.com/Patooh777/WickedHub/blob/Main/Smash%26Grab.lua?raw=true", true))()
+    
+elseif GameID == CARTYC then
+    loadstring(game:HttpGet("https://github.com/Patooh777/WickedHub/blob/Main/CarTycoon.lua?raw=true", true))()
+    
 else
     print("invalid Place")
     game.Players.LocalPlayer:Kick("Ur script dont support this shit ass game")
